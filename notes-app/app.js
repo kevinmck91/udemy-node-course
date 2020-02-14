f33a11bdee8755320680d84c0017cdec5cb4c537
Kevin Mckeon <kevin.mckeon@zurich.com>
2020-02-14 15:27:02 +0000

    Add a comment

--- notes-app/app.js
+++ notes-app/app.js
@@ -23,7 +23,7 @@ yargs.command({
 yargs.command({
     command : "remove",
     describe : "Remove a task from the list",
-    builder : {
+    builder : {     // this defines the rules of the available inputs input
         title : {
             describe : "Note Title to be Removed",
             type : 'string'
