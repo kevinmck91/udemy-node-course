8a5611a9d51368c89d0c0819c1215273125ee347
McKeon <KMK@esnet.ie>
2020-02-18 17:17:36 +0000

    Create weather app - Define proxy

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,21 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/21cd85e5ad75f5d79dbc68794b51cc71/37.8267,-122.4233'
+let requestObject = { url: 'https://api.darksky.net/forecast/21cd85e5ad75f5d79dbc68794b51cc71/53.2995,-6.1754', json:true} 
 
-request({ url: url }, (error, response) => {
-    console.log(response)
+// Configure proxy specific settings by trying to access proxy-settings.js file (only exists on work PC)
+
+try {  
+    requestObject.proxy = require('./proxy-settings.js');
+} catch(err) {
+    console.log("ERROR : Cant load the proxy settings - Not Required");
+}
+
+
+// Applicaion
+
+request( requestObject, (error, response) => {
+   // console.log(error, response);
+    //const data = JSON.parse(response.body.currently);
+     console.log(response.body.currently);
+    
 })
