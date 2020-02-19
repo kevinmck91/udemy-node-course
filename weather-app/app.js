a01c67954ab6fd2561604e81eae925c8d2110f24
Kevin Mckeon <kevin.mckeon@zurich.com>
2020-02-19 16:04:04 +0000

    Add API details for DarkSky and MapBox

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,12 @@
 const request = require('request')
 
-let requestObject = { url: 'https://api.darksky.net/forecast/21cd85e5ad75f5d79dbc68794b51cc71/53.2995,-6.1754', json:true} 
+let darkSkyRequest = { url: 'https://api.darksky.net/forecast/21cd85e5ad75f5d79dbc68794b51cc71/53.2995,-6.1754?', json:true} 
+let mapBoxRequest = { url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2V2bWNrZW9uIiwiYSI6ImNrNnRiNzRkcjBuaHYzdXQ5ZWljZjQ4MWkifQ.A3HTPlh4iRQfqd6vgI0IWg', json:true} 
 
-// Configure proxy specific settings by trying to access proxy-settings.js file (only exists on work PC)   
-
+// If work pc, search for proxy settings
 try {  
-    requestObject.proxy = require('./proxy-settings.js');
+    darkSkyRequest.proxy = require('./proxy-settings.js');
+    mapBoxRequest.proxy = require('./proxy-settings.js');
 } catch(err) {
     console.log("ERROR : Cant load the proxy settings - Not Required");
 }
@@ -13,11 +14,37 @@ try {
 
 // Applicaion
 
-request( requestObject, (error, response) => {
-   // console.log(error, response);
-    //const data = JSON.parse(response.body.currently);
-     console.log(response.body.currently);
+request( darkSkyRequest, (error, response) => {
+
+    if(error){
+        console.log("Error connecting to darkSky")
+    } else if (response.body.error){
+        console.log("Error with darkSky response data")
+    }else {
+
+        const precipProbability = (response.body.currently.precipProbability)*100
+        let temperature = response.body.currently.temperature;
+
+        // if default is 
+        if(!darkSkyRequest.url.includes("units=si")){
+            temperature = ((temperature - 32)*(5/9)).toFixed(2)
+        }
+            
+        console.log(response.body.daily.data[0].summary + " Is is currently " + temperature + " degrees out. There is a " + precipProbability + "% chance of " + response.body.currently.precipType)
     
+    }
+     
 })
 
 
+request( mapBoxRequest, (error, response) => {
+    
+    if(error){
+        console.log("Error connecting to Map Box")
+    } else if (response.body.features.length === 0){
+        console.log("Error with Map Box response data")
+    } else {
+        latitude  = response.body.features[0].center[0];
+        longitude  = response.body.features[0].center[1];
+    }
+})
