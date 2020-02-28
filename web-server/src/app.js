bd56c50a14fff048677af2a54568a00063ceadba
Kevin Mckeon <kevin.mckeon@zurich.com>
2020-02-28 12:48:25 +0000

    Complete basic  Express integration

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')        // core module
 const express = require('express')
+const hbs = require('hbs')
 
 
 // Doenst take arguments. Configured by functions
@@ -7,15 +8,21 @@ const app = express();
 
 // Define paths for Express Config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+console.log(partialsPath)
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
 
 // Setup static directiory to serve
 app.use(express.static(publicDirectoryPath))
 app.use(express.static(viewsPath))
+
 
 
 app.get('', (req, res) => {
@@ -40,6 +47,21 @@ app.get('/help', (req, res) => {
         name: 'Kevin Mckeon'
     })
 })
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title:'404 - Page Not Found',
+        details: 'Help Page not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title:'404 - Page Not Found',
+    })
+})
+
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
